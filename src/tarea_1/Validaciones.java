b8c2a9cc0c7eb49ce4cafb96d7fc3b86b9a7c807
Ruler2798 <2995432420101@ingenieria.usac.edu.gt>
2022-01-31 19:06:11 -0600

    commit_2.1

---
+++ src/tarea_1/Validaciones.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tarea_1;
+
+/**
+ *
+ * @author lphg3
+ */
+public class Validaciones {
+    oracion_1 = 
+   public void vocal (){
+       for (int i = 0; i < oracion_1.charAt(); i++) {
+           Object object = arr[i];
+           
+       }
+
+}
+
+    
+}
+
+
