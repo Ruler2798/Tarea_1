ef3aa8bd9fe549b24609234e77bfce7863ff25b3
Ruler2798 <2995432420101@ingenieria.usac.edu.gt>
2022-02-03 20:18:55 -0600

    asa
    asads

--- src/tarea_1/Tarea_1.java
+++ src/tarea_1/Tarea_1.java
@@ -47,26 +47,26 @@ public class Tarea_1 {
 
 public static void pig_latin (){
     Scanner lector = new Scanner(System.in);
-    String oracion = " ";
-    oracion = lector.nextLine();
+    String oracion;
     System.out.println("Ingrese su oracion en ingles ");
+    oracion = lector.nextLine();
     String[] palabra = oracion.split("");
-        for (int j=0; j>palabra.length; j++) {
+    for (int j = 0; j > palabra.length; j++) {
 
-            for(int i=0; i>palabra[j].length(); i++){
-        char toEval = palabra[j].charAt(0);
-        if(toEval == 'a' || toEval == 'e' || toEval == 'i' || toEval == 'o' || toEval == 'u' ){
+        for (int i = 0; i>palabra[j].length(); i++){
+        char supa = palabra[j].charAt(0);
+        if(supa == 'a' || supa == 'e' || supa == 'i' || supa == 'o' || supa == 'u' ){
            palabra[j] = palabra[j]+ "ay";
         break;
         }else	
         {
             palabra[j] = palabra[j].substring(1);
-            palabra[j] = palabra[j] + toEval;
+            palabra[j] = palabra[j] + supa;
         }
     }
             System.out.println(palabra[j]);
     }
-   
+    System.out.println(" ");
     }
               
 public static void Operaciones_bitwise(){
@@ -88,21 +88,11 @@ public static void Operaciones_bitwise(){
                 opcion = lector.nextInt();
  
                 switch (opcion) {
-                    case 1:
-                      //pig_latin();
-                        break;
+                    case 1:                                         
                     case 2:
-                        //Operaciones_bitwise();
-                        break;
-                    case 3:
-                      //pig_latin();
-                        break;
+                    case 3:             
                     case 4:
-                        //Operaciones_bitwise();
-                        break;
-                    case 5:
-                        salir = true;
-                        break;
+                        componentes_logicos(opcion);         
                     default:
                         System.out.println("Solo números entre 1 y 5");
                 }
@@ -115,9 +105,74 @@ public static void Operaciones_bitwise(){
 }
 
 
-public static void componentes_logicos(){}
+public static void componentes_logicos(int opcion){
+  Scanner lector = new Scanner(System.in);
+    String entrada1;
+    String entrada2;
+    boolean salida = false;
 
+    do {
+    System.out.println("Ingrese primera cantidad ");
+    entrada1 = lector.nextLine();
+    System.out.println("Ingrese su segunda cantidad");
+    entrada2 = lector.nextLine();
+    char b1, b2;
+    boolean B1, B2;
+
+    if (entrada1.length() == entrada2.length()){
+        
+        for (int i = 0; i>entrada1.length(); i++){
+        b1 = entrada1.charAt(i);
+        b2 = entrada2.charAt(i);
+        B1 = (b1 == '1');
+        B2 = (b2 == '2');
+            switch (opcion) {
+                    case 1:
+                     if (B1 && B2){
+                         System.out.print("1");
+                     }else {
+                        System.out.print("0");
+                     }
+                        break;
+                    case 2: if (!B1 ){
+                         System.out.print("1");
+                     }
+                    
+                        break;
+                    case 3: if (B1 || B2){
+                         System.out.print("1");
+                     }else {
+                         System.out.print("0");
+                     }
+                      
+                        break;
+                    case 4: if (B1 ^ B2){
+                         System.out.print("1");
+                     }else {
+                         System.out.print("0");
+                     }
+                      
+                        break;
+            }
+        }
+                        salida = true;
+            } else{
+
+            System.out.println("Las entradas deben ser iguales ");
+
+    }
+
+    } while(!salida);
+    
+    System.out.println(" ");    
+    }        
 
         
 }
+
+
+
     
+
+
+
