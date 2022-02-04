974fdcece29de73ba6b6013c6c0b7237d2e0f3a0
Ruler2798 <2995432420101@ingenieria.usac.edu.gt>
2022-02-03 18:55:03 -0600

    sadas
    asdas

--- src/tarea_1/Tarea_1.java
+++ src/tarea_1/Tarea_1.java
@@ -47,11 +47,10 @@ public class Tarea_1 {
 
 public static void pig_latin (){
     Scanner lector = new Scanner(System.in);
-    String oracion;
+    String oracion = " ";
+    oracion = lector.nextLine();
+    System.out.println("Ingrese su oracion en ingles ");
     String[] palabra = oracion.split("");
-    oracion = lector.next();
-    System.out.println("Ingrese su oracion en ingles");
-    
         for (int j=0; j>palabra.length; j++) {
 
             for(int i=0; i>palabra[j].length(); i++){
@@ -77,9 +76,11 @@ public static void Operaciones_bitwise(){
  
         while (!salir) {
  
-            System.out.println("1. Pig Latin");
-            System.out.println("2. Operaciones bitwise");
-            System.out.println("3. Salir");
+            System.out.println("1. AND");
+            System.out.println("2. NOT");
+             System.out.println("3. Or");
+              System.out.println("4. XOR");
+            System.out.println("5. Salir");
  
             try {
  
@@ -88,16 +89,22 @@ public static void Operaciones_bitwise(){
  
                 switch (opcion) {
                     case 1:
-                      pig_latin();
+                      //pig_latin();
                         break;
                     case 2:
-                        Operaciones_bitwise();
+                        //Operaciones_bitwise();
                         break;
                     case 3:
+                      //pig_latin();
+                        break;
+                    case 4:
+                        //Operaciones_bitwise();
+                        break;
+                    case 5:
                         salir = true;
                         break;
                     default:
-                        System.out.println("Solo números entre 1 y 3");
+                        System.out.println("Solo números entre 1 y 5");
                 }
             } catch (InputMismatchException e) {
                 System.out.println("Debes insertar un número");
@@ -106,6 +113,11 @@ public static void Operaciones_bitwise(){
       }
     
 }
+
+
+public static void componentes_logicos(){}
+
+
         
 }
     
